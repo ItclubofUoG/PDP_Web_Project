ea60113958de92961c61b91c3d2f8e4889dc91a1
trungthach020201 <wendythach489@gmail.com>
2022-06-22 22:06:43 +0700

    no ok

--- Views/UserLogs.php
+++ Views/UserLogs.php
@@ -47,7 +47,7 @@ include_once('./connectDB.php');
                     }
                     //find limit and current page
                     $current_page = isset($_GET['pages']) ? $_GET['pages'] : 1;
-                    $limit = 2;  // set the limit of line in page
+                    $limit = 1;  // set the limit of line in page
                     //calculate total page and start page
                     $total_page = ceil($total_records / $limit);
                     //limit the page from 1 to end
@@ -60,7 +60,7 @@ include_once('./connectDB.php');
                     $start = ($current_page - 1) * $limit;
                     $sql = "SELECT * FROM user_log a, user b, event c where a.student_id = b.student_id and a.event_id = c.event_id and c.event_id ='$currentEventId' LIMIT $start, $limit";
                     if (isset($_GET['func']) && $_GET['func'] == 'filter') {
-                        $sql = $_GET['sql'];
+                        $sql = $_GET['sql'] . " LIMIT $start, $limit";
                     }
                     $result = mysqli_query($conn, $sql);
                     while ($row = mysqli_fetch_array($result,  MYSQLI_ASSOC)) {
