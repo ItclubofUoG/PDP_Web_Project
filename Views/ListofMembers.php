6099ef72abf4895bdab2bc32d68880a83ffc3d4a
Thach Ngoc Trung <91512874+trungthach020201@users.noreply.github.com>
2022-06-22 13:38:09 +0700

    Done-pagination-list

--- Views/ListofMembers.php
+++ Views/ListofMembers.php
@@ -25,7 +25,7 @@
                     <th class="head-row">Course</th>
                     <th class="head-row">Score</th>
                 </tr>
-                <?php
+                 <?php
                 include("./connectDB.php");
                 include('./Libs/index.php');
                 if (isset($_POST['btn-search'])) {
@@ -36,12 +36,30 @@
                 } else {
                     $sql = "SELECT * FROM user a, major b, course c WHERE a.major_id = b.major_id and a.course_id = c.course_id";
                 }
-                $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+                // $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+                //find the total records
+                $result = mysqli_query($conn, 'select count(student_id) as total from user');
+                $row = mysqli_fetch_assoc($result);
+                $total_records = $row['total'];
+                //find limit and current page
+                $current_page = isset($_GET['pages']) ? $_GET['pages'] : 1;
+                $limit = 15;             // set the limit of line in page
+                //calculate total page and start page
+                $total_page = ceil($total_records / $limit);
+                //limit the page from 1 to end
+                if ($current_page > $total_page){
+                    $current_page = $total_page;
+                }
+                else if ($current_page < 1){
+                    $current_page = 1;
+                }
+                //find start page
+                $start = ($current_page - 1) * $limit;
+                //query and display
+                $result = mysqli_query($conn, "SELECT * FROM  user a, major b, course c WHERE a.student_id LIKE '%$search%' and a.major_id=b.major_id and a.course_id=c.course_id OR a.fullname LIKE '%$search%' and a.major_id=b.major_id and a.course_id=c.course_id LIMIT $start, $limit");
                 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
                     $student_id = $row['student_id'];
                     $res = mysqli_query($conn, "SELECT SUM(scores) as scores FROM `user_log` WHERE student_id='$student_id'");
-                    $resCourseAndMajor = mysqli_query($conn, "SELECT * FROM `user` INNER JOIN `major` ON user.major_id=major.major_id INNER JOIN `course` ON user.course_id=course.course_id WHERE student_id='$student_id'");
-                    $cowCourseAndMajor = mysqli_fetch_array($resCourseAndMajor, MYSQLI_ASSOC);
                     $rowscores = mysqli_fetch_array($res, MYSQLI_ASSOC);
                 ?>
                     <tr class="table-body">
@@ -125,6 +143,35 @@
         })
     </script>
     <!-- End script -->
+    
+    <!-- pagination page started here -->
+    <div class="pag-outline">
+            <div class="pag-block">
+           
+                <!-- display prev when not stay in page 1 -->
+                <?php if ($current_page > 1 && $total_page > 1){
+                    echo '   <a href="admin.php?pages='.($current_page-1).'">Prev</a> |';
+                }?>
+                <div class="pag-item">
+                <?php 
+                //loop the between 
+                for ($i = 1; $i <= $total_page; $i++){
+                    if ($i == $current_page){
+                        echo '<span class="pag-number" style="border: 2px solid blue; background-color:#ccc;">'.$i.'</span> | ';
+                    }
+                    else{
+                        echo '<a class="pag-hplink" href="admin.php?pages='.$i.'"><div class="pag-number">'.$i.'</div></a> |';
+                    }
+                }
+                ?>
+                </div>
+                <?php 
+                //display btn next when it not be the end page
+                if ($current_page < $total_page && $total_page > 1){
+                    echo '<a href="admin.php?page='.($current_page+1).'">Next</a> |';
+                }?>
+            </div>
+    </div>
 
 
 </body>
