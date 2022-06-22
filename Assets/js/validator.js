483b36de268a33d36dfb26e3eb935bc38c7084a1
trungthach020201 <wendythach489@gmail.com>
2022-06-22 15:55:30 +0700

    front

--- Assets/js/validator.js
+++ Assets/js/validator.js
@@ -125,7 +125,7 @@ CheckSizeImage=function(selector,form,error,formGroupSelector,message,errorclass
     var fileUpload = document.querySelector(selector);
     //Check whether the file is valid Image.
     var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(.jpg|.png|.gif)$");
-    if (regex.test(fileUpload.value.toLowerCase())) {
+    if (true) {
  
         //Check whether HTML5 is supported.
         if (typeof (fileUpload.files) != "undefined") {
