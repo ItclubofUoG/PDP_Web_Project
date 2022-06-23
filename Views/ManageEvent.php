0b887754174818573f71400699119e8df2a756a1
luuhoaiphong <luuhoaiphong147@gmail.com>
2022-06-23 15:23:40 +0700

    fix

--- Views/ManageEvent.php
+++ Views/ManageEvent.php
@@ -120,7 +120,7 @@ include_once('./connectDB.php');
                         <span class="alert-error-modal"></span>
                     </div>
                     <div class="modal-input">
-                        <input type="text" id="eventscore" name="eventscore" class="modal-input-box" placeholder="Score">
+                        <input type="number" min="0" max="100" id="eventscore" name="eventscore" class="modal-input-box" placeholder="Score">
                         <span class="alert-error-modal"></span>
                     </div>
                     <div class="modal-input">
@@ -193,7 +193,7 @@ include_once('./connectDB.php');
                         <span class="alert-error-modal"></span>
                     </div>
                     <div class="modal-input">
-                        <input type="text" id="updatescore" min="0" max="1000" name="updatescore" class="modal-input-box" placeholder="Score" value="<?php echo $row['score'] ?>">
+                        <input type="number" min="0" max="100" id="updatescore" min="0" max="1000" name="updatescore" class="modal-input-box" placeholder="Score" value="<?php echo $row['score'] ?>">
                         <span class="alert-error-modal"></span>
                     </div>
 
