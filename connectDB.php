c32ca61f41f59b27b7800104bcce3f28953a1733
Quoc Thong <thongnguyennqt@gmail.com>
2022-06-29 11:26:32 +0700

    revert

--- connectDB.php
+++ connectDB.php
@@ -3,7 +3,7 @@
 	$servername = "localhost";
     $username = "root";		
     $password = "";			
-    $dbname = "itclubof_pdp";
+    $dbname = "pdp_itclub";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
