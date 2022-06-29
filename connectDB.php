8933562a34d5a14a7efc341d9065602214df4ffd
Quoc Thong <thongnguyennqt@gmail.com>
2022-06-29 10:57:45 +0700

    chech login

--- connectDB.php
+++ connectDB.php
@@ -3,7 +3,7 @@
 	$servername = "localhost";
     $username = "root";		
     $password = "";			
-    $dbname = "pdp_itclub";
+    $dbname = "itclubof_pdp";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
