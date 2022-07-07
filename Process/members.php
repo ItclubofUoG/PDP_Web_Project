ed4d874d2e284c6faf9e389c305c36b64e6c7629
luuhoaiphong <luuhoaiphong147@gmail.com>
2022-07-07 17:23:24 +0700

    export

--- Process/members.php
+++ Process/members.php
@@ -9,7 +9,7 @@ if (isset($_POST['btn_filter'])) {
     $endDate = $_POST['endMonth'];
     $result = Get_result_querry($major, $course, $startDate, $endDate);
     $sqlFilter = $result[0];
-    $url = "../admin.php?page=home&&func=filter&&sql=$sqlFilter&&startDate=$startDate&&endDate=$endDate";
+    $url = "../admin.php?page=home&&func=filter&&sql=$sqlFilter";
     $url = str_replace(PHP_EOL, '', $url);
     header("location: $url");
 }
@@ -30,42 +30,66 @@ if (isset($_POST['btn_export'])) {
 
                     <th>ID</th>
                     <th>Name</th>
+                    <th>Email</th>                    
+                    <th>Event joined in</th>
                     <th>Scores</th>
-                    <th>Event detail</th>
-
                 </tr>
             </thead>';
         while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
             $student_id = $row['student_id'];
 
+
             if (empty($startDate) != true && empty($endDate) != true) {
                 $res_sum = mysqli_query($conn, "SELECT SUM(scores) as score FROM user_log Where student_id = '$student_id' 
                 AND  checkin_date >='$startDate'  and checkin_date <= '$endDate'") or die(mysqli_error($conn));
-            } elseif (empty($startDate) != true && empty($endDate) == true) {
 
+                $sqlEvent = mysqli_query($conn, "SELECT event_title from event a, user_log b WHERE a.event_id = b.event_id and b.student_id = '$student_id'
+                AND  checkin_date >='$startDate'  and checkin_date <= '$endDate'") or die(mysqli_error($conn));
+            } elseif (empty($startDate) != true && empty($endDate) == true) {
                 $res_sum = mysqli_query($conn, "SELECT SUM(scores) as score FROM user_log Where student_id = '$student_id' 
+                and  checkin_date >='$startDate'") or die(mysqli_error($conn));
+
+                $sqlEvent = mysqli_query($conn, "SELECT event_title from event a, user_log b WHERE a.event_id = b.event_id and b.student_id = '$student_id'
                 and  checkin_date >='$startDate'") or die(mysqli_error($conn));
             } elseif (empty($startDate) == true && empty($endDate) != true) {
                 $res_sum = mysqli_query($conn, "SELECT SUM(scores) as score FROM user_log Where student_id = '$student_id' 
                 and checkin_date <= '$endDate'") or die(mysqli_error($conn));
+
+                $sqlEvent = mysqli_query($conn, "SELECT event_title from event a, user_log b WHERE a.event_id = b.event_id and b.student_id = '$student_id'
+                and checkin_date <= '$endDate'") or die(mysqli_error($conn));
             } else {
                 $res_sum = mysqli_query($conn, "SELECT SUM(scores) as score FROM user_log Where student_id = '$student_id'") or die(mysqli_error($conn));
+                $sqlEvent = mysqli_query($conn, "SELECT event_title from event a, user_log b WHERE a.event_id = b.event_id and b.student_id = '$student_id'");
             }
             $rowScore = mysqli_fetch_array($res_sum, MYSQLI_ASSOC);
 
 
+
+            $array = array();
+            while ($rowEvent = mysqli_fetch_array($sqlEvent, MYSQLI_ASSOC)) {
+                array_push($array, $rowEvent['event_title']);
+            }
+
+
             $out .= ' 
-                        <tbody>
-                            <tr>
-                                    <td>' . $row['student_id'] . '</td>
-                                    <td>' . $row['fullname'] . '</td>
-                                    <td>' . $rowScore['score']  . '</td>
-                                    <td>' . ''  . '</td>
-                    ';
+            <tbody>
+            <tr>
+                    <td>' . $row['student_id'] . '</td>
+                    <td>' . $row['fullname'] . '</td>
+                    <td>' . $row['email'] . '</td>  
+                    <td style=min-width: fit-content>';
+            foreach ($array as $value) {
+                $out .=  $value . ' | ';
+            }
+
+            $out .= '</td>    
+                    <td>' . $rowScore['score']  . '</td>
+
+    ';
 
             $out .= '
-                </tr>
-             </tbody>';
+</tr>
+</tbody>';
         }
         header('Content-Type: application/xls');
         header('Content-Disposition: attachment; filename=User.xls');
@@ -75,18 +99,18 @@ if (isset($_POST['btn_export'])) {
 
 ?>
 <!-- Process for EnventAttendence - Search Function -->
-<?php
-if (isset($_GET['btn_search']) && $_GET['btn_search'] == "Search") {
+<!-- <?php
+        if (isset($_GET['btn_search']) && $_GET['btn_search'] == "Search") {
 
-    $month = $_GET['month'];
-    $year = $_GET['year'];
+            $month = $_GET['month'];
+            $year = $_GET['year'];
 
-    $sqlquery = "SELECT * FROM user_log WHERE Month(checkin_date) ='$month'AND Year(checkin_date)='$year'";
-    $url = "../index.php?page=attendence&&sqlquery=$sqlquery";
-    $url = str_replace(PHP_EOL, '', $url);
-    header("location: $url");
-}
-echo "<script type='text/javascript'>alert('None data');</script>";
-echo "<script> location.href='../admin.php?page=home'</script>";
-exit;
-?>
+            $sqlquery = "SELECT * FROM user_log WHERE Month(checkin_date) ='$month'AND Year(checkin_date)='$year'";
+            $url = "../index.php?page=attendence&&sqlquery=$sqlquery";
+            $url = str_replace(PHP_EOL, '', $url);
+            header("location: $url");
+        }
+        echo "<script type='text/javascript'>alert('None data');</script>";
+        echo "<script> location.href='../admin.php?page=home'</script>";
+        exit;
+        ?> -->
