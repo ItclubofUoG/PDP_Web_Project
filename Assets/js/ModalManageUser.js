ff701b5eece98663f01d34851c7b9cc80b23db10
Hao Tran <macbook@MacBooks-MacBook-Pro.local>
2022-07-15 17:15:14 +0700

    fix display update

--- Assets/js/ModalManageUser.js
+++ Assets/js/ModalManageUser.js
@@ -1,23 +1,23 @@
-const UpdateUsers = document.querySelectorAll('.js-update-user') //sellect the class use to use js
-const modalclose = document.querySelector('.js-modal-close-update-user')
-const ModalContent = document.querySelector('.js-modal-content-update-user')
-const modal = document.querySelector('.js-modal')
+const UpdateUsers = document.querySelectorAll(".js-update-user"); //sellect the class use to use js
+const modalclose = document.querySelector(".js-modal-close-update-user");
+const ModalContent = document.querySelector(".js-modal-content-update-user");
+const modal = document.querySelector(".js-modal");
 
-function showModal() {
-    modal.classList.add('open')
-}
+// function showModal() {
+//     modal.classList.add('open')
+// }
 
-for (const UpdateUser of UpdateUsers) {
-    UpdateUser.addEventListener('click', showModal)
-}
+// for (const UpdateUser of UpdateUsers) {
+//     UpdateUser.addEventListener('click', showModal)
+// }
 
 function hideModal() {
-    modal.classList.remove('open')
+  modal.classList.remove("open");
 }
-modalclose.addEventListener('click', hideModal)
+modalclose.addEventListener("click", hideModal);
 
 // modal.addEventListener('click',hideModal)
 
-ModalContent.addEventListener('click', function (event) {
-    event.stopPropagation() //stop nổi bọt
-})
+ModalContent.addEventListener("click", function (event) {
+  event.stopPropagation(); //stop nổi bọt
+});
