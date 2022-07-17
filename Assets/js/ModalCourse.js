411160d3e70719b7d4cab1c6b629310b2f456fb6
trungthach020201 <wendythach489@gmail.com>
2022-07-17 16:31:25 +0700

    Fixed

--- Assets/js/ModalCourse.js
+++ Assets/js/ModalCourse.js
@@ -1,47 +1,45 @@
- //Modal add
- const AddCourses = document.querySelectorAll('.js-add-course') //sellect the class used to use js
- const modalcloseAdd = document.querySelector('.js-modal-close-add-course')
- const ModalContentAddCourse = document.querySelector('.js-modal-content-add')
- const modalcourse = document.querySelector('.js-modal-add')
- 
- function showModalAdd() {
-     modalcourse.classList.add('open')
- }
+//Modal add
+const AddCourses = document.querySelectorAll(".js-add-course"); //sellect the class used to use js
+const modalcloseAdd = document.querySelector(".js-modal-close-add-course");
+const ModalContentAddCourse = document.querySelector(".js-modal-content-add");
+const modalcourse = document.querySelector(".js-modal-add");
 
- for(const AddCourse of AddCourses ){
-     AddCourse.addEventListener('click',showModalAdd)
- }
+function showModalAdd() {
+  modalcourse.classList.add("open");
+}
 
- function hideModalAdd(){
-     modalcourse.classList.remove('open')
- }
- modalcloseAdd.addEventListener('click',hideModalAdd)
+for (const AddCourse of AddCourses) {
+  AddCourse.addEventListener("click", showModalAdd);
+}
 
- ModalContentAddCourse.addEventListener('click', function (event) {
-     event.stopPropagation() //stop nổi bọt
- })
- 
- 
- 
- // Modal update
- const UpdateCourses = document.querySelectorAll('.js-update-course') //sellect the class used to use js
- const modalclose = document.querySelector('.js-modal-close-update-course')
- const ModalContent = document.querySelector('.js-modal-content-update')
- const modal = document.querySelector('.js-modal')
- 
- function showModal() {
-     modal.classList.add('open')
- }
+function hideModalAdd() {
+  modalcourse.classList.remove("open");
+}
+modalcloseAdd.addEventListener("click", hideModalAdd);
 
- for(const UpdateCourse of UpdateCourses ){
-     UpdateCourse.addEventListener('click',showModal)
- }
+ModalContentAddCourse.addEventListener("click", function (event) {
+  event.stopPropagation(); //stop nổi bọt
+});
 
- function hideModal(){
-     modal.classList.remove('open')
- }
- modalclose.addEventListener('click',hideModal)
+// Modal update
+const UpdateCourses = document.querySelectorAll(".js-update-course"); //sellect the class used to use js
+const modalclose = document.querySelector(".js-modal-close-update-course");
+const ModalContent = document.querySelector(".js-modal-content-update");
+const modal = document.querySelector(".js-modal");
 
- ModalContent.addEventListener('click', function (event) {
-     event.stopPropagation() //stop nổi bọt
- })
+function showModal() {
+  modal.classList.add("open");
+}
+
+for (const UpdateCourse of UpdateCourses) {
+  UpdateCourse.addEventListener("click", showModal);
+}
+
+function hideModal() {
+  modal.classList.remove("open");
+}
+modalclose.addEventListener("click", hideModal);
+
+ModalContent.addEventListener("click", function (event) {
+  event.stopPropagation(); //stop nổi bọt
+});
